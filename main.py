ce846876d1501240c6c009beb9d441be4519cb91
enzo masson <39915492+enzomasson25@users.noreply.github.com>
2020-03-12 11:29:11 +0100

    Update main.py

--- main.py
+++ main.py
@@ -16,5 +16,27 @@ for lettre in list_lettre :
    html = urlopen("http://fchd.info/index"+lettre+".htm")
    html_soup = BeautifulSoup(html, 'html.parser')
    rows = rows + html_soup.findAll("li")
+   
 
-print(len(rows))
+def find_nth(haystack, needle, n):
+    start = haystack.find(needle)
+    while start >= 0 and n > 1:
+        start = haystack.find(needle, start+len(needle))
+        n -= 1
+    return start
+
+teams = []
+
+for row in rows :
+    start_url=find_nth(str(row),"\"",1)
+    end_url=find_nth(str(row),"\"",2)
+    team=row.text
+    url=str(row)[start_url+1:end_url]
+    teams_entry = {
+                "nom": team,
+                "url": "http://fchd.info/"+url
+            } 
+    teams.append(teams_entry)  
+    
+df = pd.DataFrame(teams)
+df.to_csv("teams.csv")
