9feca5f9aaacbe9fd50e53a39d8cf7ce44e392de
Nowtryz <damien.djmb@gmail.com>
2020-04-27 11:34:37 +0200

    deploy

---
+++ precache-manifest.d68ea120e7549f5e8d3fbe332c50cb6a.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "438239686c8288471536a8a101c90b96",
+    "url": "/index.html"
+  },
+  {
+    "revision": "829cdf8496c8c895239b",
+    "url": "/static/css/2.367471c6.chunk.css"
+  },
+  {
+    "revision": "1af62c4a8fcda3738733",
+    "url": "/static/css/main.34ded6b6.chunk.css"
+  },
+  {
+    "revision": "829cdf8496c8c895239b",
+    "url": "/static/js/2.036744b3.chunk.js"
+  },
+  {
+    "revision": "1af62c4a8fcda3738733",
+    "url": "/static/js/main.af8f196f.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "bfc688ca490f0079051aea5b8ec75cbe",
+    "url": "/static/media/ayo-ogunseinde-2.bfc688ca.jpg"
+  },
+  {
+    "revision": "2051e0e847124c536df9c01a3f672032",
+    "url": "/static/media/erik-lucatero-2.2051e0e8.jpg"
+  },
+  {
+    "revision": "76ff47ae5c64ccda5af18326bb272058",
+    "url": "/static/media/joe-gardner-2.76ff47ae.jpg"
+  },
+  {
+    "revision": "e06d84cb846db2a463ac3d904ac0c362",
+    "url": "/static/media/kaci-baum-2.e06d84cb.jpg"
+  },
+  {
+    "revision": "48ac036da7c7923266266f63746fc5f1",
+    "url": "/static/media/nucleo-icons.48ac036d.ttf"
+  },
+  {
+    "revision": "5c78b4cf03771fd2287325c44f0d3cd9",
+    "url": "/static/media/nucleo-icons.5c78b4cf.woff2"
+  },
+  {
+    "revision": "6836b93bf1af0b571928d98458a68445",
+    "url": "/static/media/nucleo-icons.6836b93b.woff"
+  },
+  {
+    "revision": "f32ed5ad3d76067b8e43be1a0bd1ba91",
+    "url": "/static/media/nucleo-icons.f32ed5ad.eot"
+  },
+  {
+    "revision": "7907591946eee451e53d97d92cc238c9",
+    "url": "/static/media/react-logo.79075919.png"
+  }
+]);
